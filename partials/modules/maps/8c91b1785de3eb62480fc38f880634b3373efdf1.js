111d1709f82860ad8d0c0f7072704037f6a8ee12
Shilo <shilo@Shilos-Pro-II.local>
2019-01-06 14:53:13 +1000

    Initial version.

--- partials/modules/maps/8c91b1785de3eb62480fc38f880634b3373efdf1.js
+++
@@ -1,7 +0,0 @@
-
-(function(){
-  'use strict';
-  // Prepare the 'maps' module for subsequent registration of controllers and delegates
-  angular.module('ecclesia.maps', [ 'ngMaterial' ]);
-  
-})();
--- partials/modules/user/user.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-
-  // Prepare the 'maps' module for subsequent registration of controllers and delegates
-  angular.module('mainApp.sample', [ 'ngMaterial' ]);
-
-
-})();
